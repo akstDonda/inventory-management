54d5fe74467e75165eb52f6a0eceed42f4e60ee1
jayam04 <93824505+jayam04@users.noreply.github.com>
2023-08-03 22:08:30 +0530

    User Type Activity

---
+++ app/src/main/java/com/miniproject/inventorymanagement/UserTypeActivity.java
@@ -0,0 +1,14 @@
+package com.miniproject.inventorymanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserTypeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_type);
+    }
+}
