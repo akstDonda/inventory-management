4b32ac455c1f4084d544e9a87ebd9abb5d137e7e
akstDonda <akshitdonda117@gmail.com>
2023-08-03 22:48:28 +0530

    icon add

--- app/src/main/java/com/miniproject/inventorymanagement/MainActivity.java
+++ app/src/main/java/com/miniproject/inventorymanagement/MainActivity.java
@@ -2,13 +2,24 @@ package com.miniproject.inventorymanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn=findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,UserTypeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
