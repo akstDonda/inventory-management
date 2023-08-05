e0efc57fa2341b71a6fb0e701096935f80f3d891
akstDonda <akshitdonda117@gmail.com>
2023-08-05 22:26:20 +0530

    wth error

--- app/src/main/java/com/miniproject/inventorymanagement/UserTypeActivity.java
+++ app/src/main/java/com/miniproject/inventorymanagement/UserTypeActivity.java
@@ -3,12 +3,26 @@ package com.miniproject.inventorymanagement;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class UserTypeActivity extends AppCompatActivity {
-
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_type);
+
+        btn = findViewById(R.id.floating_action_button_admin);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(UserTypeActivity.this, "hii", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
