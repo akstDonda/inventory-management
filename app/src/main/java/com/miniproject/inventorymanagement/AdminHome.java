d097f10166c97a324adf4a9ed392e5cd5daf3a88
akstDonda <akshitdonda117@gmail.com>
2023-08-07 00:14:38 +0530

    register is complete ....login is update very soon........

---
+++ app/src/main/java/com/miniproject/inventorymanagement/AdminHome.java
@@ -0,0 +1,30 @@
+package com.miniproject.inventorymanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class AdminHome extends AppCompatActivity {
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_home);
+
+        btn=findViewById(R.id.btn_admin_logOut);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(AdminHome.this, UserTypeActivity.class));
+                finish();
+            }
+        });
+    }
+}
--- app/src/main/java/com/miniproject/inventorymanagement/AdminRegistration.java
+++ app/src/main/java/com/miniproject/inventorymanagement/AdminRegistration.java
@@ -1,14 +1,85 @@
 package com.miniproject.inventorymanagement;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
 
 public class AdminRegistration extends AppCompatActivity {
+    EditText mCompanyName, mEmail, mPassword;
+    Button rbtn;
+    TextView login_btn;
+    ProgressBar reg_progressbar;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_registration);
+
+        mCompanyName = findViewById(R.id.edt_companyname_admin_reg);
+        mEmail = findViewById(R.id.edt_email_admin_reg);
+        mPassword = findViewById(R.id.edt_Password_admin_reg);
+
+        rbtn = findViewById(R.id.btn_admin_reg);
+        reg_progressbar = findViewById(R.id.rProgressBar);
+        mAuth = FirebaseAuth.getInstance();
+
+        //user is already create so throw direct mainActivity
+        if(mAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(),AdminHome.class));
+            finish();
+        }
+        rbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email=mEmail.getText().toString().trim();
+                String password=mPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)){
+                    mEmail.setError("Email is Required");
+                    return;
+                }
+                if (TextUtils.isEmpty(password)){
+                    mPassword.setError("password is Required");
+                    return;
+                }
+                if(password.length()<6){
+                    mPassword.setError("character More Then 6 Required");
+                    return;
+                }
+                reg_progressbar.setVisibility(View.VISIBLE);
+
+                mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(AdminRegistration.this, "SucessFully Create User", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), AdminHome.class));
+                        }else{
+                            Toast.makeText(AdminRegistration.this, "Error !!!"+ Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
+                            reg_progressbar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+            }
+        });
+
+
     }
 }
