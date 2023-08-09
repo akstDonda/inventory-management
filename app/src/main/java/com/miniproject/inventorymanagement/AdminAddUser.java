b37aef1c2a1b1c8d728425c7227578d74215e282
jayam04 <93824505+jayam04@users.noreply.github.com>
2023-08-09 16:25:36 +0530

    basics of new user

--- app/src/main/java/com/miniproject/inventorymanagement/AdminAddUser.java
+++ app/src/main/java/com/miniproject/inventorymanagement/AdminAddUser.java
@@ -3,12 +3,15 @@ package com.miniproject.inventorymanagement;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class AdminAddUser extends AppCompatActivity {
+    Button addUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_add_user);
+        addUser = findViewById(R.id.btn_add_user);
     }
 }
