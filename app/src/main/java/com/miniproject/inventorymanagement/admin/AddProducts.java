428b961c293e0f6dcd10f8f4e2deb2e9fea1b2a3
akstDonda <akshitdonda117@gmail.com>
2023-08-16 01:57:27 +0530

    add Product Page

--- app/src/main/java/com/miniproject/inventorymanagement/admin/AddProducts.java
+++ app/src/main/java/com/miniproject/inventorymanagement/admin/AddProducts.java
@@ -2,15 +2,27 @@ package com.miniproject.inventorymanagement.admin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.miniproject.inventorymanagement.R;
 
 public class AddProducts extends AppCompatActivity {
-
+    Button add;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_products);
+
+
+        add=findViewById(R.id.iconButton_add_product);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), AddProductsDetaileAdd.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/miniproject/inventorymanagement/admin/AddProductsDetaileAdd.java
@@ -0,0 +1,17 @@
+package com.miniproject.inventorymanagement.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.miniproject.inventorymanagement.R;
+
+public class AddProductsDetaileAdd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_products_detaile_add);
+        this.setTitle("Product Add");
+    }
+}
