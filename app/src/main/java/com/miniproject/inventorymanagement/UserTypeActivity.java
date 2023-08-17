600252288a0bb457b8ef3caf4084de04134f1751
Daksh Roy <dakshroy787@gmail.com>
2023-08-17 16:48:16 +0530

    xml loginpage back button... open loginpaage after user click admin button... add don't have account to open admin registration page...

--- app/src/main/java/com/miniproject/inventorymanagement/UserTypeActivity.java
+++ app/src/main/java/com/miniproject/inventorymanagement/UserTypeActivity.java
@@ -9,16 +9,16 @@ import android.view.View;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class UserTypeActivity extends AppCompatActivity {
-    FloatingActionButton btn;
+    FloatingActionButton btnadmin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_type);
-        btn=findViewById(R.id.floating_action_button_admin);
-        btn.setOnClickListener(new View.OnClickListener() {
+        btnadmin=findViewById(R.id.floating_action_button_admin);
+        btnadmin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(UserTypeActivity.this, AdminRegistrationActivity.class);
+                Intent intent = new Intent(UserTypeActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
         });
