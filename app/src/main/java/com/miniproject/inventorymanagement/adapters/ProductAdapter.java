17e40f3ccf77ffe6f80dff78f8d511831fa4c537
jayam04 <jayampatel04@gmail.com>
2023-08-31 23:07:26 +0530

    updated product adapter

--- app/src/main/java/com/miniproject/inventorymanagement/adapters/ProductAdapter.java
+++ app/src/main/java/com/miniproject/inventorymanagement/adapters/ProductAdapter.java
@@ -3,9 +3,11 @@ package com.miniproject.inventorymanagement.adapters;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.miniproject.inventorymanagement.R;
 import com.miniproject.inventorymanagement.firebase.Product;
 
@@ -14,7 +16,59 @@ import org.checkerframework.checker.nullness.qual.NonNull;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-
+//
+//public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ViewHolder> {
+//
+//    private List<Product> productList;
+//
+//    public ProductAdapter(Map<String, Product> productsMap) {
+//        // Convert the Map values to a List
+//        productList = new ArrayList<>(productsMap.values());
+//    }
+//
+//    public void updateData(Map<String, Product> updatedProductsMap) {
+//        productList.clear();
+//        productList.addAll(updatedProductsMap.values());
+//        notifyDataSetChanged();
+//    }
+//
+//    @NonNull
+//    @Override
+//    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_product, parent, false);
+//        return new ViewHolder(view);
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+//        Product product = productList.get(position);
+//        // Bind the product data to the ViewHolder's UI components
+//        holder.bind(product);
+//    }
+//
+//    @Override
+//    public int getItemCount() {
+//        return productList.size();
+//    }
+//
+//    public static class ViewHolder extends RecyclerView.ViewHolder {
+//        // Declare your UI components here and initialize them in the constructor
+//
+//        public ViewHolder(@NonNull View itemView) {
+//            super(itemView);
+//            // Initialize your UI components here
+//        }
+//
+//        public void bind(Product product) {
+//            // Bind the product data to your UI components
+//            // For example:
+//            // productNameTextView.setText(product.getName());
+//            // productDescriptionTextView.setText(product.getDescription());
+//            // ...
+//        }
+//    }
+//}
+//
 public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ViewHolder> {
 
     private List<Product> productList;
@@ -22,12 +76,6 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ViewHold
     public ProductAdapter(Map<String, Product> productsMap) {
         // Convert the Map values to a List
         productList = new ArrayList<>(productsMap.values());
-    }
-
-    public void updateData(Map<String, Product> updatedProductsMap) {
-        productList.clear();
-        productList.addAll(updatedProductsMap.values());
-        notifyDataSetChanged();
     }
 
     @NonNull
@@ -40,7 +88,6 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ViewHold
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Product product = productList.get(position);
-        // Bind the product data to the ViewHolder's UI components
         holder.bind(product);
     }
 
@@ -50,20 +97,24 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ViewHold
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        // Declare your UI components here and initialize them in the constructor
+        // Declare your UI components here
+        private TextView productNameEditText;
+        private TextView productDescriptionEditText;
+        // ... other UI components
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             // Initialize your UI components here
+            productNameEditText = itemView.findViewById(R.id.productNameTextView);
+            productDescriptionEditText = itemView.findViewById(R.id.productCurrentStockTextView);
+            // ... initialize other UI components
         }
 
         public void bind(Product product) {
-            // Bind the product data to your UI components
-            // For example:
-            // productNameTextView.setText(product.getName());
-            // productDescriptionTextView.setText(product.getDescription());
-            // ...
+            // Bind product data to UI components
+            productNameEditText.setText(product.getName());
+            productDescriptionEditText.setText(Integer.toString(product.getStockIn() - product.getStockOut()));
+            // ... bind other data
         }
     }
 }
-
