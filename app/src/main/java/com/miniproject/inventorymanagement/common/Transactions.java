9779e0de033d147e437b37372e8df47739dea3be
jayam04 <jayampatel04@gmail.com>
2023-09-01 16:19:49 +0530

    add transaction layout

--- app/src/main/java/com/miniproject/inventorymanagement/common/Transactions.java
+++ app/src/main/java/com/miniproject/inventorymanagement/common/Transactions.java
@@ -4,15 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import java.util.Date;
 import com.miniproject.inventorymanagement.R;
 import com.miniproject.inventorymanagement.adapters.ProductAdapter;
 import com.miniproject.inventorymanagement.adapters.TransactionAdapter;
+import com.miniproject.inventorymanagement.admin.AddNewTransaction;
 import com.miniproject.inventorymanagement.firebase.DatabaseHandler;
 
 public class Transactions extends AppCompatActivity {
     DatabaseHandler dbhandler;
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,5 +36,14 @@ public class Transactions extends AppCompatActivity {
         TransactionAdapter adapter = new TransactionAdapter(dbhandler.getTransactions());
         recyclerView.setAdapter(adapter);
 
+        btn = findViewById(R.id.addTransactionButton);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Transactions.this, AddNewTransaction.class);
+                startActivity(intent);
+            }
+        });
     }
 }
