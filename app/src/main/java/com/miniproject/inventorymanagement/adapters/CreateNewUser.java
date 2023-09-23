6e3e7891f445080a44a430dd0537335b62fc3935
Daksh Roy <dakshroy787@gmail.com>
2023-09-23 11:03:50 +0530

    some warning solving..

--- app/src/main/java/com/miniproject/inventorymanagement/adapters/CreateNewUser.java
+++ app/src/main/java/com/miniproject/inventorymanagement/adapters/CreateNewUser.java
@@ -1,41 +1,24 @@
 package com.miniproject.inventorymanagement.adapters;
 
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.app.ProgressDialog;
-import android.content.Intent;
-import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.material.textfield.TextInputEditText;
-import com.google.android.material.textfield.TextInputLayout;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentChange;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.miniproject.inventorymanagement.R;
-import com.miniproject.inventorymanagement.adapters.EmpAdapter;
-import com.miniproject.inventorymanagement.firebase.User;
 
 import java.util.ArrayList;
-import java.util.Objects;
 
 public class CreateNewUser extends AppCompatActivity {
 
@@ -78,6 +61,7 @@ public class CreateNewUser extends AppCompatActivity {
         db.collection("employeeDetails")
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @SuppressLint("NotifyDataSetChanged")
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         empArrayList.clear();
--- app/src/main/java/com/miniproject/inventorymanagement/common/UserLogin.java
+++ app/src/main/java/com/miniproject/inventorymanagement/common/UserLogin.java
@@ -5,15 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.miniproject.inventorymanagement.R;
 
 public class UserLogin extends AppCompatActivity {
 
+    TextInputLayout employeeemail,employeepassword;
+    EditText empe,empp;
     ImageButton elg_back_btn;
     TextView elg_reggt;
+    Button btnemplogin;
+    FirebaseAuth mauth;
+    FirebaseFirestore db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +31,23 @@ public class UserLogin extends AppCompatActivity {
 
         elg_back_btn=findViewById(R.id.elogin_back_btn);
         elg_reggt=findViewById(R.id.eopenreg);
+
+        employeeemail=findViewById(R.id.empemail);
+        employeepassword=findViewById(R.id.emppass);
+        empe=employeeemail.getEditText();
+        empp=employeepassword.getEditText();
+
+        mauth=FirebaseAuth.getInstance();
+        db=FirebaseFirestore.getInstance();
+
+
+
+
+
+
+
+
+
 
         // back to login and go to registration of employee
         elg_back_btn.setOnClickListener(new View.OnClickListener() {
