c88dce0d7d1e7c571fed21baca456644b0da02c4
akstDonda <akshitdonda117@gmail.com>
2023-10-01 18:33:29 +0530

    Settings button works...........

--- app/src/main/java/com/miniproject/inventorymanagement/common/Settings.java
+++ app/src/main/java/com/miniproject/inventorymanagement/common/Settings.java
@@ -3,16 +3,22 @@ package com.miniproject.inventorymanagement.common;
 import static android.app.PendingIntent.getActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.miniproject.inventorymanagement.R;
 
 public class Settings extends AppCompatActivity {
 
+    CardView aboutUs;
+    TextView txtAU,txtCU,txtTC,txtfb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,7 +26,77 @@ public class Settings extends AppCompatActivity {
 
         setTitle("  Setting");
 
+        txtAU = findViewById(R.id.txtau);
+        txtCU = findViewById(R.id.txtcu);
+        txtTC = findViewById(R.id.txttc);
+        txtfb=findViewById(R.id.txtfb);
         Button logutButton = findViewById(R.id.logoutButton);
+
+
+        txtAU.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://lathiyaparth.github.io/aboutUs/";
+
+                // Create an Intent to open a web browser with the specified URL.
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+
+                // Start the web browser activity.
+                startActivity(intent);
+            }
+        });
+
+        txtCU.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                String url = "https://lathiyaparth.github.io/contactUs/";
+
+                // Create an Intent to open a web browser with the specified URL.
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+
+                // Start the web browser activity.
+                startActivity(intent);
+
+
+            }
+        });
+
+
+        txtTC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                String url = "https://lathiyaparth.github.io/termcondion/";
+
+                // Create an Intent to open a web browser with the specified URL.
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+
+                // Start the web browser activity.
+                startActivity(intent);
+
+
+            }
+        });
+
+        txtfb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                String url = "https://lathiyaparth.github.io/feedback/";
+
+                // Create an Intent to open a web browser with the specified URL.
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+
+                // Start the web browser activity.
+                startActivity(intent);
+
+
+            }
+        });
 
         logutButton.setOnClickListener(view -> {
             FirebaseAuth.getInstance().signOut();
